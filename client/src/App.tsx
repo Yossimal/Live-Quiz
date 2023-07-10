c7985914cbc210c78b86585035406dd96cc5c9c6
Noam shushan <noam8shu@gmail.com>
2023-07-10 10:25:58 +0300

    AuthProvider context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { AuthProvider } from './contexts/AuthProvider'
 import Singup from "./pages/Singup"
+
+const queryClient = new QueryClient()
 
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Singup />} />
-        <Route path="/about" element={<h1>About</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Singup />} />
+            <Route path="/about" element={<h1>About</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
